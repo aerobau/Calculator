b46e307428ff75d0eeb4a65e29b4a17f2a02d515
Alexander Robau <aerobau@gmail.com>
2015-11-10 21:32:03 -0500

    Adding helper methods to the Visitor class

---
+++ Calculator/visitor.cpp
@@ -0,0 +1,30 @@
+//
+//  visitor.cpp
+//  Calculator
+//
+//  Created by Alexander Robau on 11/10/15.
+//  Copyright © 2015 Robau inc. All rights reserved.
+//
+
+#include "visitor.h"
+#include "math_element.h"
+
+// Multiply two MathElements by having the second accept the MultiplicationVisitor of the first
+MathElementPtr Visitor::Multiply(const MathElement *first, const MathElement *second) const {
+    return second->Accept(first->CreateMultiplicationVisitor());
+}
+
+// Divide two MathElements by having the second accept the DivisionVisitor of the first
+MathElementPtr Visitor::Divide(const MathElement* first, const MathElement* second) const {
+    return second->Accept(first->CreateDivisionVisitor());
+}
+
+// Add two MathElements by having the second accept the AdditionVisitor of the first
+MathElementPtr Visitor::Add(const MathElement* first, const MathElement* second) const {
+    return second->Accept(first->CreateAdditionVisitor());
+}
+
+// Subtract two MathElements by having the second accept the SubtractionVisitor of the first
+MathElementPtr Visitor::Subtract(const MathElement* first, const MathElement* second) const {
+    return second->Accept(first->CreateSubtractionVisitor());
+}
--- Calculator/visitor.h
+++ Calculator/visitor.h
@@ -23,12 +23,30 @@ typedef std::unique_ptr<MathElement> MathElementPtr;
 
 class Visitor {
   public:
+    // VISITING METHODS //
+    // Visitation methods for all of the different types that are suppported for symbolic
+    // representation by the calculator, note that they all take constant pointers, the ownership
+    // of the objects will be elsewhere.  These are meant to be overriden and depending on the
+    // visitor subclass, which is specific to element subclass and function being performed, the
+    // proper function will be performed.
+    
     virtual MathElementPtr VisitInteger(const Integer*) const { return MathElementPtr(nullptr); }
     virtual MathElementPtr VisitDecimal(const Decimal*) const { return MathElementPtr(nullptr); }
     virtual MathElementPtr VisitFraction(const Fraction*) const { return MathElementPtr(nullptr); }
     virtual MathElementPtr VisitVariable(const Variable*) const { return MathElementPtr(nullptr); };
     virtual MathElementPtr VisitMultiplicationExpression(const MultiplicationExpression*) const { return MathElementPtr(nullptr); };
     virtual MathElementPtr VisitAdditionExpression(const AdditionExpression*) const { return MathElementPtr(nullptr); };
+    
+protected:
+    // VISITOR SPECIFIC HELPER METHODS //
+    // Helper methods for all visitor subclasses that will multiply two constant MathElement
+    // pointers, note that the actual ownership of the objects will be elsewhere by unique_ptr
+    // conventions, these methods are final and implemented in the cpp file for visitor
+    
+    MathElementPtr Multiply(const MathElement*, const MathElement*) const;
+    MathElementPtr Divide(const MathElement*, const MathElement*) const;
+    MathElementPtr Add(const MathElement*, const MathElement*) const;
+    MathElementPtr Subtract(const MathElement*, const MathElement*) const;
 };
 
 #endif // VISITOR_H_
